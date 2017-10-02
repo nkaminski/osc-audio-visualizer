271a83db024d3ef4f2fb5f03cee128510b26478b
nkaminski <nashkaminski@comcast.net>
2017-10-02 15:03:29 -0500

    Debug shell script

--- run_pulseaudio.sh
+++ run_pulseaudio.sh
@@ -4,7 +4,7 @@ OSC_PORT="7700"
 NOISEFLOOR="-40"
 
 if [ -n "$1" ] ; then
-    pacmd list-sources
-else
     pacat -r -d $1 --channels=2 --format=s16le | ./osc-audio-proc -a $OSC_ADDR -p $OSC_PORT -n $NOISEFLOOR
+else
+    pacmd list-sources
 fi
